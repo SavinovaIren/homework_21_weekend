0b6217d2bdad177374087cc29bdc2d46a0bb339e
SavinovaIren <irka887@gmail.com>
2022-08-11 23:08:13 +0300

    if не работает

--- clases.py
+++ clases.py
@@ -31,40 +31,39 @@ class Storage(ABC):
 class Store(Storage):
 
     def __init__(self, items: dict, capacity: int = 100):
-        self.__items = items
-        self.__capacity = capacity
+        super().__init__(items, capacity)
 
     def __str__(self):
-        list = ""
-        for k, v in self.__items.items():
-            list += f"{k}:{v}\n"
-        return list
+        list_ = ""
+        for k, v in self.items.items():
+            list_ += f"{k}:{v}\n"
+        return list_
 
     def get_free_space(self):  # вернуть количество свободных мест
         current_space = 0
-        for value in self.__items.values():
+        for value in self.items.values():
             current_space += value
-        return self.__capacity - current_space
+        return self.capacity - current_space
 
     def add(self, title, quantity: int):  # увеличивает запас items с учетом лимита capacity
-        if title in self.__items.keys():
+        if title in self.items.keys():
             if self.get_free_space() >= int(quantity):
-                self.__items[title] += int(quantity)
+                self.items[title] += int(quantity)
                 return True
             else:
                 print("Места на складе недостаточно")
                 return False
         else:
             if self.get_free_space() >= int(quantity):
-                self.__items[title] = int(quantity)
+                self.items[title] = int(quantity)
                 return True
             else:
                 print("Места на складе недостаточно")
                 return False
 
     def remove(self, title, quantity: int):  # уменьшает запас items но не ниже 0
-        if self.__items[title] > int(quantity):
-            self.__items[title] -= int(quantity)
+        if self.items[title] > int(quantity):
+            self.items[title] -= int(quantity)
             return True
         else:
             print("На складе недостаточно товара")
@@ -74,7 +73,7 @@ class Store(Storage):
         return f"{self.items}"
 
     def get_unique_items_count(self):  # возвращает количество уникальных товаров.
-        return len(self.__items.keys())
+        return len(self.items.keys())
 
 
 class Shop(Store):
@@ -93,45 +92,130 @@ class Shop(Store):
 
 class Request:
 
-    def __init__(self, request_str):
-        """Доставить 3 печеньки из склад в магазин"""
-        req_str = request_str.split()
-        action = req_str[0]
-        self.__count = req_str[1]
-        self.__product = req_str[2]
-        if action == "Добавить":
-            self.__from = req_str[4]
-            self.__to = req_str[6]
-        elif action == "Забрать":
-            self.__from = req_str[4]
-            self.__to = None
-        elif action == "Привезти":
-            self.__to = req_str[4]
-            self.__from = None
+    def __init__(self, request_str: str):
 
-    def move(self):
-        if self.__to and self.__from:
-            if eval(self.__to).add(self.__product, self.__count):
-                eval(self.__from).remove(self.__product, self.__count)
-        elif self.__to:
-            eval(self.__to).add(self.__product, self.__count)
-        elif self.__from:
-            eval(self.__to).remove(self.__product, self.__count)
+        """Доставить 3 яблоко из склад-2 в магазин"""
+        self.request_str = request_str
+        request_list = self.request_str.split()
+        self.__count = int(request_list[1])
+        self.__product = request_list[2]
+        self.__from = request_list[4]
+        self.__to = request_list[6]
+
+    @property
+    def from_(self):
+        return self.__from
+
+    @from_.setter
+    def from_(self, value):
+        self.__from = value
+
+    @property
+    def to_(self):
+        return self.__to
+
+    @to_.setter
+    def to_(self, value):
+        self.__to = value
+
+    @property
+    def count(self):
+        return self.__count
+
+    @count.setter
+    def count(self, value):
+        self.__count = value
+
+    @property
+    def product(self):
+        return self.__product
+
+    @product.setter
+    def product(self, value):
+        self.__product = value
+
+    def __repr__(self):
+        return f"Доставить {self.__count} {self.__product} из {self.__from} в {self.__to}"
 
 
 storage_1 = Store(items={"яблоко": 10, "слива": 20, "банан": 30})
 storage_2 = Store(items={"ананас": 15, "слива": 20, "арбуз": 30, "груша": 30})
 shop_1 = Shop(items={"арбуз": 2, "ананас": 3, "яблоко": 1, "груша": 1})
 
-print("Привет!")
 
-while True:
-    user_input = input("Введите запрос:\n")
+def move():
+    name_storage_1 = "cклад-1"
+    name_storage_2 = "cклад-2"
+    shop = "магазин"
+    print("Программа для логистики товара между складами и магазинами запущена")
+    while True:
+        user_input = input("Нажмите 'ок', чтобы продолжить, если желаете завершить - 'стоп':\n")
+        if user_input == "стоп":
+            break
+        elif user_input == 'ок':
+            print("Количество и вид товара:")
+            print(f"Склад-1:\n{storage_1}")
+            print(f"Склад-2:\n{storage_2}")
+            print(f"Магазин:\n{shop_1}")
+            us_input = input("Доставить ___ __ из __ в ___:\n")
+            us = us_input.split()
+            user_req = f"Доставить {us[1]} {us[2]} из {us[4]} в {us[6]}"
+            req = Request(user_req)
+            if req.from_.lower() == name_storage_1:
+                if storage_1.remove(req.product, req.count):
+                    print("Нужное количество есть на складе")
+                    if shop_1.add(req.product, req.count):
+                        print(f"Доставить {req.count} {req.product} с {req.from_} в {req.to_}")
+                        print(f"Успешно доставлено в {req.to_}")
+                        print(storage_1)
+                        print(shop_1)
+                    else:
+                        print("В магазине недостаточно места, попробуйте что-то другое")
+                        storage_1.add(req.product, req.count)
+                else:
+                    print("На складе нет нужного количества товара или нет такого наименования товара")
+            elif req.from_.lower() == name_storage_2:
+                if storage_2.remove(req.product, req.count):
+                    print("Нужное количество есть на складе")
+                    if shop_1.add(req.product, req.count):
+                        print(f"Доставить {req.count} {req.product} с {req.from_} в {req.to_}")
+                        print(f"Успешно доставлено в {req.to_}")
+                        print(storage_1)
+                        print(shop_1)
+                    else:
+                        print("В магазине недостаточно места, попробуйте что-то другое")
+                        storage_1.add(req.product, req.count)
+                else:
+                    print("На складе нет нужного количества товара или нет такого наименования товара")
+            elif req.from_.lower() == shop:
+                if req.to_.lower() == name_storage_1:
+                    if shop_1.remove(req.product, req.count):
+                        print("Нужное количество есть в магазине")
+                        if storage_1.add(req.product, req.count):
+                            print(f"Доставить {req.count} {req.product} с {req.from_} в {req.to_}")
+                            print(f"Успешно доставлено в {req.to_}")
+                            print(storage_1)
+                            print(shop_1)
+                        else:
+                            print("В магазине недостаточно места, попробуйте что-то другое")
+                            shop_1.add(req.product, req.count)
+                    else:
+                        print("В магазине нет нужного количества товара или нет такого наименования товара")
+                elif req.to_.lower() == name_storage_2:
+                    if shop_1.remove(req.product, req.count):
+                        print("Нужное количество есть в магазине")
+                        if storage_2.add(req.product, req.count):
+                            print(f"Доставить {req.count} {req.product} с {req.from_} в {req.to_}")
+                            print(f"Успешно доставлено в {req.to_}")
+                            print(storage_2)
+                            print(shop_1)
+                        else:
+                            print("В магазине недостаточно места, попробуйте что-то другое")
+                            shop_1.add(req.product, req.count)
+                    else:
+                        print("В магазине нет нужного количества товара или нет такого наименования товара")
+            else:
+                print(f"Такого склада нет в списке либо название склада введено не корректно {req.from_.lower()} {name_storage_1}")
 
-    if user_input == "стоп":
-        break
-    else:
-        req = Request(user_input)
-        req.move()
-print(storage_1)
-print(shop_1)
+
+move()
